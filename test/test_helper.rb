3d0d9399ff12bdd3a2ca9b1b9b3c747aceb4659b
Alice D <alicedilanchian@gmail.com>
2020-11-30 08:00:20 -0500

    test_helper updated

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,5 +17,28 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def setup
+    OmniAuth.config.test_mode = true
+  end
+
+  def mock_auth_hash(user)
+    return {
+      provider: user.provider,
+      uid: user.uid,
+      info: {
+        email: user.email,
+        nickname: user.username
+      }
+    }
+  end
+
+  def perform_login(user = nil)
+    user ||= User.first
+
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+    get auth_callback_path(:github)
+
+    return user
+  end
+
 end
