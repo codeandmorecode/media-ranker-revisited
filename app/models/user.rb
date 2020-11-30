821652a912532c2bab481866f94c8b6bde929957
Alice D <alicedilanchian@gmail.com>
2020-11-29 23:32:41 -0500

    user method created ccalled build_from_github

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
   validates :uid, uniqueness: {scope: :provider}
   validates :username, presence: true
 
+  def self.build_from_github(auth_hash)
+    user = User.new
+    user.uid = auth_hash["uid"]
+    user.provider = auth_hash["provider"]
+    user.username = auth_hash["info"]["nickname"]
+    user.email = auth_hash["info"]["email"]
+    return user
+  end
 end
